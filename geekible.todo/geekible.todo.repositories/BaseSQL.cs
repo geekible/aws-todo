f1233008abef61c5474408d4f6290c74cfc175f9
Richard Bennett <richardbennett@geekible.org>
2023-02-20 10:14:09 +0000

    base repo
    base repository

---
+++ geekible.todo/geekible.todo.repositories/BaseSQL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geekible.todo.repositories
+{
+    public class BaseSQL<T> where T : class
+    {
+        private ApplicationDbContext ctx;
+
+        public BaseSQL()
+        {
+            ctx =         
+        }
+    }
+}
---
+++ geekible.todo/geekible.todo.repositories/ConfigureDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace geekible.todo.repositories
+{
+    internal class ConfigureDbContext
+    {
+        internal static ApplicationDbContext Context
+        {
+            get
+            {
+                var builder = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json");
+                var config = builder.Build();
+                var context = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseSqlServer(config.GetConnectionString("DefaultConnection"));
+                return new ApplicationDbContext(context.Options);
+            }
+        }
+    }
+}
